e4f38e93c9b9b280607aff87482c488e4e4c72e5
Marijan Zivic <marijan.zivic@gmail.com>
2018-08-15 01:37:21 +0200

    New sample data and new node svg css

--- src/assets/si-sample-location.ts
+++ src/assets/si-sample-location.ts
@@ -1,11 +1,11 @@
 const dataTreeSimple= {
     "result": [
-      {"id":"1","description":"Service Location","nodeIcon": "\uf015", "nodeSymbol": "square"},
-      {"id":"2","description":"Cable Access (FTTH)","parent":"1","nodeIcon": "\uf2cb", "nodeSymbol": "square"},
-      {"id":"3","description":"3","parent":"2"},
-      {"id":"4","description":"4","parent":"2"},
-      {"id":"5","description":"5","parent":"2"},
-      {"id":"6","description":"6","parent":"5"},
+      {"id":"1","description":"Service Location","nodeIcon": "\uf015", "nodeSymbol": "square", "nodeColor" : ""},
+      {"id":"2","description":"Cable Access (FTTH)","parent":"1","nodeIcon": "\uf22c", "nodeSymbol": "square", "nodeColor" : ""},
+      {"id":"3","description":"Digital TV","parent":"2","nodeIcon": "\uf26c", "nodeSymbol": "square", "nodeColor" : ""},
+      {"id":"4","description":"Internet","parent":"2","nodeIcon": "\uf1eb", "nodeSymbol": "square"},
+      {"id":"5","description":"Voice","parent":"2","nodeIcon": "\uf095", "nodeSymbol": "square"},
+      {"id":"6","description":"Internet Speed 20/5 Mbit/s","parent":"4","nodeIcon": "\uf496", "nodeSymbol": "square"},
       {"id":"7","description":"7","parent":"5"},
       {"id":"8","description":"8","parent":"5"},
       {"id":"9","description":"9","parent":"3"}
